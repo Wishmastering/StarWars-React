86f8456b8df0bfb8f9755cd6a61c93e0428d1df2
Wishmastering <charsdb@gmail.com>
2022-09-17 09:50:21 +0000

    Probando FETCH Characters

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,65 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+
+			characters: [] ,
+			planets: [] ,
+			vehicles: [] ,
+			favorites: [] , 
+			// demo: [
+			// 	{
+			// 		title: "FIRST",
+			// 		background: "white",
+			// 		initial: "white"
+			// 	},
+			// 	{
+			// 		title: "SECOND",
+			// 		background: "white",
+			// 		initial: "white"
+			// 	}
+			// ]
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
+			// loadSomeData: () => {
+			// 	/**
+			// 		fetch().then().then(data => setStore({ "foo": data.bar }))
+			// 	*/
+			// },
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+				
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			
+			getCharacters: () => {
+				fetch("https://www.swapi.tech/api/people")
+				.then(res => res.json())
+				.then(data => console.log(data))
+				.catch(err => console.error(err))
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
+			GetIdCharacters: (id) => {
+				fetch("https://www.swapi.tech/api/people/"+id)
+				.then(res => res.json())
+				.then(data => console.log(data))
+				.catch(err => console.error(err))
+			},
+
 			}
 		}
 	};
-};
+// };
 
 export default getState;
