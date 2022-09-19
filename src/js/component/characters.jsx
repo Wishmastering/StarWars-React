9844d757c744cab02a4e4a59bf2ce348784538ec
Wishmastering <charsdb@gmail.com>
2022-09-19 23:34:06 +0000

    Carta mostrada exitosamente con foto correcta, PENDIENTE ATRIBUTOS EN CARTA / A:ADIR FAVORITOS

--- src/js/component/characters.jsx
+++ src/js/component/characters.jsx
@@ -2,28 +2,39 @@ import React, {useContext} from "react"  // #1 import useContext de React
 import { Link } from "react-router-dom";
 import {Context} from "../store/appContext.jsx"; // #2 Traer el context
 
-
+ // #3 Llamar el context
 const Characters = () => {
-
+    const {store, actions}= useContext(Context)
+    // console.log(useContext(Context));
+        
 	return (
-        <div className="card container col-4 mx-auto" style={{width: 18 + 'rem'}}>
-            <img src={"https://phantom-marca.unidadeditorial.es/c22cf319021ecf8b02d0157bb9bbb817/resize/660/f/webp/assets/multimedia/imagenes/2022/05/25/16535003789582.jpg"} 
-                 className="card-img-top" alt="Baby Rigo"/>
-            <div className="card-body">
-                <h5 className="card-title"> <b>Luke Skywalker </b> </h5> 
-                <p className="card-text"> Gender: Male</p>
-                <p className="card-text"> Hair Color: None</p>
-                <p className="card-text"> Eye Color: Blue</p>
-                 <Link to="/learnmore">
-                    <a href="#" className="btn btn-primary float-start">Learn More!</a>
-				 </Link>
+         <>
+            {store.charactersCard.map((charCard) => { 
+                return(
+            <div className="card container col-4 mx-auto" style={{width: 18 + 'rem'}}>
+                <img src={"https://starwars-visualguide.com/assets/img/characters/"+ charCard.uid +".jpg"} 
+                    className="card-img-top" alt="Baby Rigo"/>
+                <div className="card-body">
+                    <h5 className="card-title"> <b>{charCard.name} </b> </h5> 
+                    <p className="card-text"> {charCard.gender}</p>
+                    <p className="card-text"> {charCard.hair_color}</p>
+                    <p className="card-text"> {charCard.eye_color}</p>
+                    <Link to="/learnmore">
+                        <a href="#" className="btn btn-primary float-start">Learn More!</a>
+                    </Link>
 
-                <p className="fa fa-heart text-danger float-end"></p>
-                
+                    <p className="fa fa-heart text-danger float-end"></p>
+                    
+                </div>
             </div>
-        </div>
-	);
-};
+                
+                    )                       
+                                            }
+                                )
+            }
+         </>
+            )
+                         }
 
 export default Characters
 
--- src/js/store/flux.jsx
+++ src/js/store/flux.jsx
@@ -2,52 +2,19 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 
-			characters: [] ,
+			charactersCard: [] ,
+			charDetails: [],
 			planets: [] ,
 			vehicles: [] ,
 			favorites: [] , 
-			// demo: [
-			// 	{
-			// 		title: "FIRST",
-			// 		background: "white",
-			// 		initial: "white"
-			// 	},
-			// 	{
-			// 		title: "SECOND",
-			// 		background: "white",
-			// 		initial: "white"
-			// 	}
-			// ]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			// exampleFunction: () => {
-			// 	getActions().changeColor(0, "green");
-			// },
-			// loadSomeData: () => {
-			// 	/**
-			// 		fetch().then().then(data => setStore({ "foo": data.bar }))
-			// 	*/
-			// },
-			// changeColor: (index, color) => {
-			// 	//get the store
-			// 	const store = getStore();
 
-			// 	//we have to loop the entire demo array to look for the respective index
-			// 	//and change its color
-			// 	const demo = store.demo.map((elm, i) => {
-			// 		if (i === index) elm.background = color;
-			// 		return elm;
-			// 	});
-				
-			// 	//reset the global store
-			// 	setStore({ demo: demo });
-			
 			getCharacters: () => {
-				fetch("https://www.swapi.tech/api/people")
+				fetch("https://www.swapi.tech/api/people/")
 				.then(res => res.json())
-				.then(data => console.log(data))
-				.catch(err => console.error(err))
+				.then(data => setStore({charactersCard: data.results}))
+				
 			},
 
 			GetIdCharacters: (id) => {
@@ -60,6 +27,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 			}
 		}
 	};
-// };
+
 
 export default getState;
